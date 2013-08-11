6462b55dc7a49410095d8af0b16bd1de649dcfb2
Joe Wanat <joseph.wanat@webfilings.com>
2013-08-11 09:47:15 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,14 +10,21 @@ describe "StaticPages" do
 
 	    it "should have the title 'Home'" do
 	    	visit '/static_pages/home'
-	    	page.should have_selector('h1', :text => 'Sample App')
+	    	page.should have_selector('title',
+	    						:text => 'Ruby on Rails Tutorial Sample App | Home')
 		end
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_content('Help')
+		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+	    	page.should have_selector('title',
+	    						:text => 'Ruby on Rails Tutorial Sample App | Help')
 		end
 	end
 
