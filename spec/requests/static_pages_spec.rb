9f9e3a75fe58dd29a113c3ca74269913cfbd2cb5
Joe Wanat <joseph.wanat@webfilings.com>
2013-08-11 10:05:37 -0500

    Added a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,7 +18,7 @@ describe "StaticPages" do
 	describe "Help page" do
 		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the title 'Help'" do
@@ -31,7 +31,20 @@ describe "StaticPages" do
 	describe "About page" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', :text => 'About Us')
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Content'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+								:text => 'Ruby on Rails Tutorial Sample App | Contact')
 		end
 	end
 end
